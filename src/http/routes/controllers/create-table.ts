5e23bf38f435bc943f1409f977498757085c71da
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2025-07-04 14:43:39 -0300

    feat: gerar qrcode das mesas cadastradas

--- src/http/routes/controllers/create-table.ts
+++ src/http/routes/controllers/create-table.ts
@@ -2,26 +2,46 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { prisma } from '../../../lib/prisma.ts'
 
+import { fileURLToPath } from 'node:url'
+import QRCode from 'qrcode'
+import path from 'node:path'
+
 const schemaBodyReqeust = z.object({
   numberTable: z.string(),
   capacity: z.string(),
-  qrcodePath: z.string().optional(),
 })
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export async function createTable(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
   try {
-    const { numberTable, capacity, qrcodePath } = schemaBodyReqeust.parse(
-      request.body
+    const { numberTable, capacity } = schemaBodyReqeust.parse(request.body)
+
+    const qrcodeUrl = `http://localhost:5173/qrcode/${numberTable}`
+
+    const qrcodeFileName = `qrcode-${numberTable}.png`
+
+    const qrcodePath = path.resolve(
+      __dirname,
+      '../../../../uploads/qrcodes',
+      qrcodeFileName
     )
+
+    await QRCode.toFile(qrcodePath, qrcodeUrl, {
+      errorCorrectionLevel: 'H',
+      type: 'png',
+      width: 400,
+    })
 
     const table = await prisma.table.create({
       data: {
         numberTable,
         capacity,
-        qrcodePath,
+        qrcodePath: qrcodeFileName,
       },
     })
 
